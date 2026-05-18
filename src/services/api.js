035968a2df22fa5893fa94791175e5053f4b6340
Norakneath25 <norakneath230505@gmail.com>
2026-05-19 00:23:01 +0700

    add api service

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+const url = "https://fakestoreapi.com/products";
+
+export const request = async (endpoint) => {
+  try {
+    const response = await fetch(`${url}${endpoint}`);
+    if (!response.ok) {
+      throw new Error(`API error: ${response.status}`);
+    }
+    return response.json();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
